b7655c1bcb9ae814bc0a412f92bc14ee2f29e070
Loisteg <loisteg@outlook.com>
2022-07-09 14:11:44 +0300

    Script is ready

---
+++ checkingPassword.js
@@ -0,0 +1,42 @@
+const arrayWithInfo = [
+  "a 1-5: abcdj",
+  "z 2-4: asfalseiruqwo",
+  "b 3-6: bhhkkbbjjjb",
+];
+
+let statusPasswords = [];
+
+arrayWithInfo.forEach((item, i) => {
+  checkPassword(item, i);
+});
+
+function checkPassword(string, idx) {
+  let counter = 0;
+  let [key, numbers, password] = string.split(" ");
+  const [min, max] = numbers.slice(0, -1).split("-");
+
+  for (let i = 0; i < password.length; i++) {
+    if (password[i] === key) counter++;
+  }
+
+  counter >= min && counter <= max
+    ? statusPasswords.push({
+        comment: `№${idx + 1} password is valid`,
+        value: 1,
+      })
+    : statusPasswords.push({
+        comment: `№${idx + 1} password isn't valid`,
+        value: 0,
+      });
+}
+
+function countValidPasswords(array) {
+  return array
+    .filter((item) => item.value === 1)
+    .reduce((acc, item) => (acc += item.value), 0);
+}
+
+const result = ` ${countValidPasswords(
+  statusPasswords
+)} valid passwords were provided`;
+console.log(result);
